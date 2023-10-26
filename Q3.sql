f813a590e86692971c85c0f1dfcc76fca31d9a1f
Emirhan Cakil <emir76aydin44@gmail.com>
2023-10-26 23:39:14 +0300

    Advanced SQL is Finished!
    Self-Join added, Some simple questions added.

---
+++ Q3.sql
@@ -0,0 +1,4 @@
+SELECT * FROM cd.facilities
+
+/* How can you retrieve all the information from the cd.facilities table?
+*/
---
+++ Q5.sql
@@ -0,0 +1,5 @@
+SELECT *  FROM cd.facilities
+WHERE membercost != 0
+
+/* How can you produce a list of
+facilities that charge a fee to members?*/
